8282d7a46088d6ffabcd0127f4228cd91ae29626
sdu2011 <550786402@qq.com>
2020-12-31 13:40:18 +0800

    增加对SMOKEPredictor的调试和注释

--- smoke/modeling/heads/smoke_head/smoke_predictor.py
+++ smoke/modeling/heads/smoke_head/smoke_predictor.py
@@ -22,7 +22,7 @@ def get_channel_spec(reg_channels, name):
         s = sum(reg_channels[:3])
         e = sum(reg_channels)
 
-    return slice(s, e, 1)
+    return slice(s, e, 1)  #class slice(start, stop[, step])
 
 #
 @registry.SMOKE_PREDICTOR.register("SMOKEPredictor")
@@ -32,7 +32,7 @@ class SMOKEPredictor(nn.Module):
 
         classes = len(cfg.DATASETS.DETECT_CLASSES)
         regression = cfg.MODEL.SMOKE_HEAD.REGRESSION_HEADS
-        regression_channels = cfg.MODEL.SMOKE_HEAD.REGRESSION_CHANNEL
+        regression_channels = cfg.MODEL.SMOKE_HEAD.REGRESSION_CHANNEL #(1,2,3,2) means (δz, δxc δyc, δh δw δl, sin α cos α) z用于预测depth或者说距离,xc,yc是距离中心点的偏移,h,w,l是维度,a是角度.
         head_conv = cfg.MODEL.SMOKE_HEAD.NUM_CHANNEL
         norm_func = _HEAD_NORM_SPECS[cfg.MODEL.SMOKE_HEAD.USE_NORMALIZATION]
 
@@ -43,6 +43,9 @@ class SMOKEPredictor(nn.Module):
 
         self.dim_channel = get_channel_spec(regression_channels, name="dim")
         self.ori_channel = get_channel_spec(regression_channels, name="ori")
+
+        # dim_channel=slice(3, 6, 1),ori_channel=slice(6, 8, 1)
+        print('dim_channel={},ori_channel={}'.format(self.dim_channel,self.ori_channel))
 
         # 分类
         self.class_head = nn.Sequential(
@@ -92,14 +95,18 @@ class SMOKEPredictor(nn.Module):
 
         # features shape:torch.Size([1, 64, 96, 320]),head_class shape:torch.Size([1, 3, 96, 320]),head_regression shape:torch.Size([1, 8, 96, 320])
         print('features shape:{},head_class shape:{},head_regression shape:{}'.format(features.shape,head_class.shape,head_regression.shape))
-        
+
         head_class = sigmoid_hm(head_class)
+
+        print('dim_channel={},ori_channel={}'.format(self.dim_channel,self.ori_channel))
         # (N, C, H, W)
         offset_dims = head_regression[:, self.dim_channel, ...].clone()
-        head_regression[:, self.dim_channel, ...] = torch.sigmoid(offset_dims) - 0.5
+        head_regression[:, self.dim_channel, ...] = torch.sigmoid(offset_dims) - 0.5 # (δz, δxc δyc, δh δw δl, sin α cos α)中的δh δw δl
+        print('offset_dims shape={}'.format(offset_dims.shape))
 
         vector_ori = head_regression[:, self.ori_channel, ...].clone()
-        head_regression[:, self.ori_channel, ...] = F.normalize(vector_ori)
+        head_regression[:, self.ori_channel, ...] = F.normalize(vector_ori) # (δz, δxc δyc, δh δw δl, sin α cos α)中的sin α cos α
+        print('vector_ori shape={}'.format(vector_ori.shape))
 
         return [head_class, head_regression]
 
